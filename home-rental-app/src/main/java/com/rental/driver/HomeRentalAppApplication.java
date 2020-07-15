afd33c688b1c48939ecdd5c1e875bce0b7ac4a59
jpark1914 <parkerj4321@gmail.com>
2020-07-15 09:48:39 -0500

    Spring Starter project added

---
+++ home-rental-app/src/main/java/com/rental/driver/HomeRentalAppApplication.java
@@ -0,0 +1,13 @@
+package com.rental.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRentalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeRentalAppApplication.class, args);
+	}
+
+}
---
+++ home-rental-app/src/test/java/com/rental/driver/HomeRentalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeRentalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
