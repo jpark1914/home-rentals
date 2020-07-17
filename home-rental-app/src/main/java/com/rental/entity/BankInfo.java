2b510f847eb0eb3db1d40ca0f2703022a3efe378
Anas Mohamad Daud <anasdaud1996@gmail.com>
2020-07-17 12:21:25 -0400

    deleted db files

---
+++ home-rental-app/src/main/java/com/rental/entity/BankInfo.java
@@ -0,0 +1,5 @@
+package com.rental.entity;
+
+public class BankInfo {
+
+}
--- home-rental-app/src/main/resources/data.sql
+++ home-rental-app/src/main/resources/data.sql
@@ -1,4 +1,1 @@
-DROP TABLE IF EXISTS RENTAL_USERS;
-INSERT INTO RENTAL_USERS VALUES(3, 'maurieg@gmail.com', 'StevieRay5!');
-INSERT INTO RENTAL_USERS VALUES(1, 'parkerj4321@gmail.com', 'superthunderkitten1914');
-INSERT INTO RENTAL_USERS VALUES(2, 'anasdaud1996@gmail.com', 'password');
+SELECT * FROM RENTAL_USERS;
