72d8bd26b9cec9d85722a03725dfb607b503ebf8
Gautam <gramasu000@citymail.cuny.edu>
2020-07-21 15:38:58 -0400

    Made a list of mock-houses in landing page

--- home-rental-angular/src/app/page-components/landing-page/landing-page.component.ts
+++ home-rental-angular/src/app/page-components/landing-page/landing-page.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  houseList : object[] = [
+    {
+      price: "$1,420",
+      address1: "4787 Yale Rd",
+      address2: "Memphis, Tennessee 38128-3709, United States",
+      numBed: 3,
+      numBath: 2,
+      sqFeet: 2100,
+      type: "Single Family House"
+    },
+    {
+      price: "$1,420",
+      address1: "4787 Yale Rd",
+      address2: "Memphis, Tennessee 38128-3709, United States",
+      numBed: 3,
+      numBath: 2,
+      sqFeet: 2100,
+      type: "Single Family House"
+    },
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 10; i++) {
+      this.houseList.push({
+        price: "$1,420",
+        address1: "4787 Yale Rd",
+        address2: "Memphis, Tennessee 38128-3709, United States",
+        numBed: 3,
+        numBath: 2,
+        sqFeet: 2100,
+        type: "Single Family House"
+      });
+    }
   }
 
 }
