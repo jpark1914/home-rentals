1821f05c72c5491e9a8135adf29edf070ee00153
jpark1914 <parkerj4321@gmail.com>
2020-08-03 09:34:41 -0500

    Working on form submission

--- home-rental-app/src/main/java/com/rental/controller/PersonalInfoController.java
+++ home-rental-app/src/main/java/com/rental/controller/PersonalInfoController.java
@@ -3,6 +3,7 @@ package com.rental.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class PersonalInfoController {
 	}
 	
 	@PostMapping(value="/save")
-	public void savePersonalInfo(PersonalInfo pi) {
+	public void savePersonalInfo(@RequestBody PersonalInfo pi) {
 		pis.savePersonalInfo(pi);
 	}
 }
