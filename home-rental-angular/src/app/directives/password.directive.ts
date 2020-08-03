da4002e6aea9c18959d4d8335eee340ebcc23af5
Gautam <gramasu000@citymail.cuny.edu>
2020-08-03 10:28:27 -0400

    Small change - removed padding and margin settings from directive definition

--- home-rental-angular/src/app/directives/password.directive.ts
+++ home-rental-angular/src/app/directives/password.directive.ts
@@ -12,9 +12,12 @@ export class PasswordDirective {
   }
 
   setup() {
-    this.el.nativeElement.setAttribute("class", "flex-fill border-0 p-0 my-0 ml-0 mr-1");
-    this.el.nativeElement.setAttribute("type", "password");
     
+    const par = this.el.nativeElement.parentNode;
+    par.setAttribute("class", par.getAttribute("class") + " d-flex border border-dark bg-white");
+    
+    this.el.nativeElement.setAttribute("class", "flex-fill border-0 p-0 my-0 ml-0 mr-1");
+    this.el.nativeElement.setAttribute("type", "password"); 
     
     const img = document.createElement('img');
     img.setAttribute("src", "../../assets/eye-slash.svg");
@@ -29,13 +32,7 @@ export class PasswordDirective {
         img.setAttribute("src", "../../assets/eye-slash.svg");
       }
     });
-    //svg.setAttribute("class", "bi bi-sys-slash-fill");
-    //svg.setAttribute("fill", "currentColor");
-    //svg.style.cssText = "width: 30px; height: 30px;";
-    //svg.innerHTML = this.eyefillslash;
     
-    const par = this.el.nativeElement.parentNode;
-    par.setAttribute("class", "d-flex border border-dark p-2 mt-2 mb-3 bg-white");
     par.appendChild(img);
   }
   
