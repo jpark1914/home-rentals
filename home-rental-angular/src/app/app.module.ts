58147330d69d929e190d3484eaca03646d4d1249
Gautam <gramasu000@citymail.cuny.edu>
2020-08-10 14:41:12 -0400

    Separated list of properties in landing page to its own component

--- home-rental-angular/src/app/app.module.ts
+++ home-rental-angular/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { PasswordDirective } from './directives/password.directive';
 import { StorageServiceModule } from 'ngx-webstorage-service';
 import { LoginPageComponent } from './page-components/login-page/login-page.component';
 import { AlertComponent } from './components/alert/alert.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { AlertComponent } from './components/alert/alert.component';
     PasswordDirective,
     LoginPageComponent,
     AlertComponent,
+    PropertyListComponent,
   ],
   imports: [
     BrowserModule,
