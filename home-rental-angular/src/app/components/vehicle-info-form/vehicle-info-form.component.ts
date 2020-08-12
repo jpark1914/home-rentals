b540aedf93fb8b6e5981833d9e15be6bfd89e289
Gautam <gramasu000@citymail.cuny.edu>
2020-08-12 13:54:40 -0400

    Added a basic form for vehicleInfo - still not connected to backend

--- home-rental-angular/src/app/components/vehicle-info-form/vehicle-info-form.component.ts
+++ home-rental-angular/src/app/components/vehicle-info-form/vehicle-info-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VehicleInfo } from 'src/app/interfaces/vehicleInfo.interface';
 
 @Component({
   selector: 'app-vehicle-info-form',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-info-form.component.css']
 })
 export class VehicleInfoFormComponent implements OnInit {
+
+  vehicleInfo : VehicleInfo = {
+    carId: null,
+    userId: null,
+    make: "",
+    model: "",
+    year: null,
+    color: "",
+    tagNumber: null,
+    state: "",
+    rentalUser: null
+  }
+
+  submitVehicleInfo() {
+    console.log(this.vehicleInfo);
+  }
 
   constructor() { }
 
