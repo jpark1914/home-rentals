ac0d7b5941befcaf9f533a742e93f375d0849ef5
Gautam <gramasu000@citymail.cuny.edu>
2020-09-10 11:50:04 -0400

    Changes to references controller/service methods

--- home-rental-app/src/main/java/com/rental/controller/ReferencesController.java
+++ home-rental-app/src/main/java/com/rental/controller/ReferencesController.java
@@ -8,13 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rental.entity.References;
+import com.rental.entity.RentalUsers;
 import com.rental.service.ReferencesService;
 import com.rental.service.RentalUserService;
 
@@ -32,19 +35,42 @@ public class ReferencesController {
 		this.rus = rus;
 	}
 	
-	@PostMapping(value = "/save")
-	public String saveRefs(@RequestBody References[] references) {
-		return refS.saveRef(references);
+	@PostMapping(value = "/add")
+	public ResponseEntity<String> addRefs(@RequestBody List<References> references, @AuthenticationPrincipal UserDetails user) {
+		RentalUsers rentalUser = this.rus.findUserByUserDetails(user);
+		String result = this.refS.addRefs(references, rentalUser);
+		if (result.equals(ReferencesService.TOO_MANY_REFS)) {
+			return ResponseEntity.status(409).body(result);
+		}
+		return ResponseEntity.ok(result);
+	}
+	
+	@PutMapping(value = "/update")
+	public ResponseEntity<String> updateRefs(@RequestBody List<References> references, @AuthenticationPrincipal UserDetails user) {
+		RentalUsers rentalUser = rus.findUserByUserDetails(user);
+		String result = refS.updateRefs(references, rentalUser);
+		if (result.equals(ReferencesService.NUM_REF_MISMATCH)) {
+			return ResponseEntity.status(409).body(result);
+		}
+		return ResponseEntity.ok(result);
+		
 	}
 	
 	@GetMapping(value="/get")
 	public ResponseEntity<List<References>> getPersonalInfo(@AuthenticationPrincipal UserDetails user) {
-		long userId = rus.findUserByEmail(user.getUsername()).getUserId();
-		Optional<List<References>> opReferences = refS.getRef(userId);
-		if (opReferences.isPresent()) {
-			return ResponseEntity.ok(opReferences.get());
+		RentalUsers rentalUser = this.rus.findUserByUserDetails(user);
+		List<References> references = refS.getRef(rentalUser);
+		if (references.size() != 0) {
+			return ResponseEntity.ok(references);
 		} else {
 			return ResponseEntity.noContent().build();
 		}
+	}
+	
+	@DeleteMapping(value="/delete")
+	public ResponseEntity<String> deleteRefs(@RequestBody List<References> references, @AuthenticationPrincipal UserDetails user) {
+		RentalUsers rentalUser = this.rus.findUserByUserDetails(user);
+		String result = refS.deleteRef(references, rentalUser);
+		return ResponseEntity.ok(result);
 	}
 }
--- home-rental-app/src/main/java/com/rental/service/ReferencesService.java
+++ home-rental-app/src/main/java/com/rental/service/ReferencesService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rental.entity.References;
+import com.rental.entity.RentalUsers;
 import com.rental.repo.ReferencesRepo;
 import com.rental.repo.RentalUserRepo;
 
@@ -26,21 +27,52 @@ public class ReferencesService {
 		this.rur = rur;
 	}
 	
-	public String saveRef(References[] ref) {
-		System.out.println(ref[0].getUserId());
-		refR.deleteAllByUserId(ref[0].getUserId());
-		List<References> theRefs = new ArrayList<References>();
-		for(References r: ref) {
-			r.setRentalUser(rur.findById(r.getUserId()).get());
-			theRefs.add(r);
+	private static final Integer MAX_REFS = 10;
+	public static final String SAVE_SUCCESS =  "Success. References Saved.";
+	public static final String TOO_MANY_REFS = "Fail. User cannot have more than" +  MAX_REFS + " references.";
+	
+	public String addRefs(List<References> refs, RentalUsers rentalUser) {
+		List<References> oldRefs = this.refR.findReferencesOfUser(rentalUser.getUserId());
+		if (oldRefs.size() == MAX_REFS) {
+			return TOO_MANY_REFS;
 		}
-		refR.saveAll(theRefs);
-		return "References saved";
+		for (References ref : refs) {
+			ref.setRefId(null);
+			ref.setRentalUser(rentalUser);
+		}
+		refR.saveAll(refs);
+		return SAVE_SUCCESS;
 	}
 	
-	public Optional<List<References>> getRef(long userId) {
-		List<References> theRefs = refR.findReferencesOfUser(userId);
-		return Optional.ofNullable(theRefs);
+	public static final String NUM_REF_MISMATCH = "Fail. Number of References do not correspond.";
+	
+	public String updateRefs(List<References> refs, RentalUsers rentalUser) {
+		List<References> oldRefs = this.refR.findReferencesOfUser(rentalUser.getUserId());
+		if (oldRefs.size() != refs.size()) {
+			return NUM_REF_MISMATCH;
+		}
+		for (int i = 0; i < oldRefs.size(); i++) {
+			refs.get(i).setRefId(oldRefs.get(i).getRefId());
+			refs.get(i).setRentalUser(rentalUser);
+		}
+		refR.saveAll(refs);
+		return SAVE_SUCCESS;
+	}
+	
+	public static final String DELETE_SUCCESS =  "Success. References Deleted.";
+	
+	public String deleteRef(List<References> refs, RentalUsers rentalUser) {
+		List<References> oldRefs = this.refR.findReferencesOfUser(rentalUser.getUserId());
+		for (References ref : refs) {
+			if (oldRefs.contains(ref)) {
+				refR.delete(ref);
+			}
+		}
+		return DELETE_SUCCESS;
+	}
+	
+	public List<References> getRef(RentalUsers rentalUser) {
+		return refR.findReferencesOfUser(rentalUser.getUserId());
 	}
 	
 }
